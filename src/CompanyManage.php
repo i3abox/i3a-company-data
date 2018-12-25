e039735b9cc49fda91fc68327360f1832d621619
overnick <overnic@httpd.cc>
2018-12-25 15:45:27 +0800

    first commit

---
+++ src/CompanyManage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: overnic
+ * Date: 2018/12/25
+ * Time: 15:39
+ */
+
+namespace I3A\Sdk\Company;
+
+
+use I3A\Base\Package\PackageManage;
+
+/**
+ * Class CompanyManage
+ * @package I3A\Sdk\Company
+ */
+class CompanyManage extends PackageManage
+{
+
+}
---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: overnic
+ * Date: 2018/12/25
+ * Time: 15:41
+ */
+
+return [
+    'aliyun' => [],
+    'shujia' => []
+];
