d1b90367737310202b5bd995eb75107e3ab4e964
jinjun <757258777@qq.com>
2018-12-28 19:00:20 +0800

    修改阿里数据结构,以及配置参数修改

--- src/Aliyun/DataBase.php
+++ src/Aliyun/DataBase.php
@@ -42,7 +42,7 @@ class DataBase extends CompanyBase
     public function decrypt($data, $token = null)
     {
         // 获取到传入的token
-        $token = $token ?? $this->app->config->get('aliyun.enterprise.token');
+        $token = $token ?? $this->app->config->get('aliyun.token');
         // 得到一个16位用于解密的key
         $key = strlen($token) >= 16 ?
             substr($token,0,16) :
@@ -70,7 +70,7 @@ class DataBase extends CompanyBase
         return base64_encode(hash_hmac(
             'sha1',
             $data,
-            $this->app->config->get('aliyun.enterprise.access_secret'),
+            $this->app->config->get('aliyun.access_secret'),
             true
         ));
     }
@@ -99,7 +99,6 @@ class DataBase extends CompanyBase
             'verify' => false,
             'http_errors' => false,
         ];
-        
         // 只需要路径部分
         $signUrl = parse_url($url, PHP_URL_PATH);
         // 加密的字符串
@@ -112,12 +111,106 @@ class DataBase extends CompanyBase
             empty($query) ? $signUrl : $signUrl.'?'.http_build_query($query)
             ]
         ));
-
         // 认证的签名
         $options['headers']['Authorization'] = "Dataplus ".
-            $this->config['enterprise']['access_key_id'].
+            $this->app->config->get('aliyun.access_key_id').
             ':'
             .$this->getSign($string);
         return $this->getHttpClient()->request($method , $url, $options);
     }
+
+    /**
+     * 格式化阿里云图谱的数据格式
+     *
+     * @param array $company
+     * @return array
+     */
+    public function setData(array $company = [])
+    {
+        //$company = json_decode($data, true);
+
+        // 企业基本信息
+        $basic = [
+            'basic' => array_get($company,
+                'result.result.module.reportModuleMap.BASIC.categoryMap.BASIC.reportPropertiesMap'),
+            // 公司股东
+            'shareholder' => array_get($company,
+                'result.result.module.reportModuleMap.BASIC.categoryMap.BASIC_SHAREHOLDER.reportPropertiesMap'),
+            // 公司分支机构
+            'branch'=> array_get($company,
+                'result.result.module.reportModuleMap.BASIC.categoryMap.BASIC_BRANCH.reportPropertiesMap'),
+            // 公司主要成员
+            'manager' => array_get($company,
+                'result.result.module.reportModuleMap.BASIC.categoryMap.BASIC_MANAGER.reportPropertiesMap')
+        ];
+        // 对外投资
+        $inv = [
+            //企业对外投资
+            'company' => array_get($company,
+                'result.result.module.reportModuleMap.INV.categoryMap.INV_ENTERPRISE.reportPropertiesMap'),
+            //法定代表人对外投资
+            'person' => array_get($company,
+                'result.result.module.reportModuleMap.INV.categoryMap.INV_LEGAL_PERSON.reportPropertiesMap')
+        ];
+        // 风险
+        $risk = [
+            // 股权风险－股权冻结
+            'equity_freeze' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_EQUITY_FREEZE.reportPropertiesMap'),
+            // 司法风险－失信公告
+            'announcement' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_EXECUTION_ANNOUNCEMENT.reportPropertiesMap'),
+            // 财产风险－动产抵押
+            'mortgage' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_MORTGAGE.reportPropertiesMap'),
+            // 财产风险－动产抵押物
+            'mortgage_subject' =>  array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_MORTGAGE_SUBJECT.reportPropertiesMap'),
+            // 变更信息
+            'change' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_CHANGE_DETAIL.reportPropertiesMap'),
+            // 行政处罚
+            'punishment' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_PUNISHMENT.reportPropertiesMap'),
+            // 经营异常
+            'operation' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_OPERATION_EXCEPTION.reportPropertiesMap'),
+            // 司法风险－执行公告
+            'punish' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_PUNISH_BREAK.reportPropertiesMap'),
+            // 股权风险－股权出质
+            'pledged' => array_get($company,
+                'result.result.module.reportModuleMap.RISK.categoryMap.RISK_EQUITY_PLEDGED.reportPropertiesMap'),
+
+        ];
+        // 对外任职
+        $position = array_get($company,
+            'result.result.module.reportModuleMap.EXTERNAL_POSITION.categoryMap.EXTERNAL_POSITION_LEGAL_PERSON.reportPropertiesMap');
+        // 年报
+        $financial = array_get($company, 'FINANCIAL');
+
+        // 招聘
+        $recurit = [
+            'recurit' => array_get($company, 'RECURIT'),
+            'quota' => array_get($company, 'RECURIT_QUOTA'),
+            'statistic' => array_get($company, 'RECURIT_STATISTIC'),
+
+        ];
+        // 信誉
+        $credit = array_get($company,
+            'result.result.module.reportModuleMap.CREDIT.categoryMap.CREDIT_RATE.reportPropertiesMap');
+
+        // 把内容按type 对应的值组成数组
+        $companyInfo = [
+            'financial' => $financial,
+            'recurit' => $recurit,
+            'basic' => $basic,
+            'credit' => $credit,
+            'inv' => $inv,
+            'external' => $position,
+            'risk' => $risk,
+        ];
+        return $companyInfo;
+    }
+
 }
--- src/I3A/Client.php
+++ src/I3A/Client.php
@@ -22,7 +22,31 @@ class Client extends ClientBase
     {
         return $this->request('gateway/api/company/map', [
             'com' => $com
-        ]);
+        ], 'GET');
+    }
+
+    /**
+     * 激活企业图谱服务
+     *
+     * @param $params
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function register($params = [])
+    {
+        return $this->request('gateway/api/gateway.api.company.map/open', $params);
+    }
+
+    /**
+     * 判断是否激活企业图谱
+     *
+     * @param array $params
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function hasRegister($params = [])
+    {
+        return $this->request('gateway/api/gateway.api.company.map/has', $params, 'GET');
     }
 
 
